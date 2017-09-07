9bf3b3c6be10d096d346532554876ce030b40e5c
Alexander <alex.filippov.w@gmail.com>
2017-09-07 18:06:47 +0300

    Lesson 4

--- Lessons/Lesson4_2.py
+++ Lessons/Lesson4_2.py
@@ -1,7 +1,5 @@
 import pytest
 from selenium import webdriver
-from selenium.common.exceptions import NoSuchElementException
-from selenium.webdriver.common.by import By
 
 @pytest.fixture
 def driver(request):
@@ -9,12 +7,9 @@ def driver(request):
     request.addfinalizer(wd.quit)
     return wd
 
-def are_elements_present(driver, *args):
-    return len(driver.find_elements(*args)) = 1
-
 def test(driver):
-    driver.implicitly_wait(10)
     driver.get("http://localhost/litecart/")
-    form = driver.find_element_by_id("box-most-popular")
-    form.find_element_by_link_text("Green Duck")
-    are_elements_present(driver, By.CLASS_NAME, "sticker new")
+    ducks = driver.find_elements_by_class_name("product")
+    for i in range (len(ducks)):
+        print(ducks)
+        assert len(ducks[i].find_elements_by_class_name("sticker")) == 1
